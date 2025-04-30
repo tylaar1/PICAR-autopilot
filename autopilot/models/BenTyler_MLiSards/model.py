a0596d50503491ba7e41de3b62bf8b8767d9263e
benatkinsonx <142601279+benatkinsonx@users.noreply.github.com>
2025-04-30 19:09:43 +0100

    new predict method in model.py

--- autopilot/models/BenTyler_MLiSards/model.py
+++ autopilot/models/BenTyler_MLiSards/model.py
@@ -16,14 +16,30 @@ class Model:
         im = tf.expand_dims(im, axis=0)
         return im
 
+    ## ORIGINAL
+    # def predict(self, image):
+    #     angles = np.arange(17)*5+50
+    #     image = self.preprocess(image)
+        
+    #     pred_speed = self.speed_model.predict(image)[0]
+    #     speed = pred_speed[0].astype(int)*35
+    #     pred_angle = self.angle_model.predict(image)[0]
+    #     angle = angles[np.argmax(pred_angle)]
+    #     print('angle:', angle,'speed:', speed)
+        
+    #     return angle, speed
+    
+    # FROM TOM
     def predict(self, image):
-        angles = np.arange(17)*5+50
-        image = self.preprocess(image)
-        
-        pred_speed = self.speed_model.predict(image)[0]
-        speed = pred_speed[0].astype(int)*35
-        pred_angle = self.angle_model.predict(image)[0]
-        angle = angles[np.argmax(pred_angle)]
-        print('angle:', angle,'speed:', speed)
-        
-        return angle, speed
+        processed_image = self.preprocess(image)
+        predictions = [self.speed_model(processed_image, training=False),
+                       self.angle_model(processed_image, training=False)]
+        speed_prediction = predictions[0].numpy()
+        angle_prediction = predictions[1].numpy()
+
+        speed_value = float(speed_prediction[0][0])
+        angle_value = float(angle_prediction[0][0])
+
+        angle = 80 * angle_value + 50
+        speed = 35 * speed_value
+        return angle,speed
